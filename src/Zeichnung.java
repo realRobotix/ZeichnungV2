72270411e336cee2c873d4dec3ef86730b7e6932
Julian Haarmann <julian.h.haarmann@gmail.com>
2022-08-26 12:23:39 +0200

    add randomColor

--- src/Zeichnung.java
+++ src/Zeichnung.java
@@ -1,6 +1,8 @@
 import java.awt.Color;
+import java.util.Random;
 
 public class Zeichnung {
+
     private final Square square;
 
     public Zeichnung() {
@@ -8,13 +10,16 @@ public class Zeichnung {
     }
 
     public void zeichne() {
-        this.square.draw();
-
         while (true) {
-            this.square.morph(50, 100, 600, 75, Color.green, 1000, 2);
-            this.square.morph(100, 50, 575, 600, Color.blue, 1000, 2);
-            this.square.morph(50, 100, 100, 575, Color.green, 5000, 2);
-            this.square.morph(100, 50, 75, 100, Color.red, 5000, 2);
+            square.morph(50, 100, 600, 75, getRandomColor(), 2500, 2);
+            square.morph(100, 50, 575, 600, getRandomColor(), 2500, 2);
+            square.morph(50, 100, 100, 575, getRandomColor(), 2500, 2);
+            square.morph(100, 50, 75, 100, getRandomColor(), 2500, 2);
         }
+    }
+
+    public Color getRandomColor() {
+        Random random = new Random();
+        return new Color(random.nextInt(256), random.nextInt(256), random.nextInt(256));
     }
 }
