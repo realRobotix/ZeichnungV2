a0b2594ad93edf2ef2823ee213fd1d09132d2897
Julian Haarmann <julian.h.haarmann@gmail.com>
2022-09-04 18:01:23 +0200

    refactoring && use randomColor for initial square

--- src/Zeichnung.java
+++ src/Zeichnung.java
@@ -6,19 +6,20 @@ public class Zeichnung {
     private final Square square;
 
     public Zeichnung() {
-        this.square = new Square(100, 50, 75, 100, Color.red);
+        this.square = new Square(100, 50, 75, 100, randomColor());
     }
 
     public void zeichne() {
+        //noinspection InfiniteLoopStatement
         while (true) {
-            square.morph(50, 100, 600, 75, getRandomColor(), 2500, 2);
-            square.morph(100, 50, 575, 600, getRandomColor(), 2500, 2);
-            square.morph(50, 100, 100, 575, getRandomColor(), 2500, 2);
-            square.morph(100, 50, 75, 100, getRandomColor(), 2500, 2);
+            square.morph(50, 100, 600, 75, randomColor(), 2500, 2);
+            square.morph(100, 50, 575, 600, randomColor(), 2500, 2);
+            square.morph(50, 100, 100, 575, randomColor(), 2500, 2);
+            square.morph(100, 50, 75, 100, randomColor(), 2500, 2);
         }
     }
 
-    public Color getRandomColor() {
+    public Color randomColor() {
         Random random = new Random();
         return new Color(random.nextInt(256), random.nextInt(256), random.nextInt(256));
     }
